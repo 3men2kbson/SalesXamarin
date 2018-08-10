72a425bfbd648d3d0f863cac7cf214fa533ccbfc
Lazaro R. Grullon Salasar <lgrullon@hidroelectrica.gov.do>
2018-08-09 20:43:16 -0400

    Primer commit
    Primer commit

---
+++ SalesXamarin.BackEnd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SalesXamarin.BackEnd.Startup))]
+namespace SalesXamarin.BackEnd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
